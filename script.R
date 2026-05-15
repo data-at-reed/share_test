409b26b46c25304322505382a6ed7d165712c2ff
Josie Griffin <griffinj@rstudio.reed.edu>
2026-05-15 14:25:27 -0700

    again

--- script.R
+++ script.R
@@ -2,6 +2,6 @@ x <- "Hi, Paul"
 print(x)
 
 # did PAT work? not with gitcreds_set()
-# do not know! Perhaps it will!
+# do not know! Perhaps it will! maybe
 
 usethis::use_git_config(credential.helper = "store")
